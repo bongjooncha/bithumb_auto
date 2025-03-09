1ca2c06bc38d4754c4e3ddb8535814737daa70fd
hiddenbebb <cbj4133@naver.com>
2025-03-09 11:22:53 +0900

    sell buy 변수 수정

--- sell_buy.py
+++ sell_buy.py
@@ -4,6 +4,8 @@ import python_bithumb
 
 result = {}
 coin = "ONDO"
+price = 1530
+amount = 100
 result["decision"] = "buy"
 
 access = os.getenv("BITHUMB_ACCESS_KEY")
@@ -16,6 +18,7 @@ print(result["decision"])
 
 if result["decision"] == "buy":
     # print(bithumb.buy_market_order("KRW-"+coin, 550_000))
-    print(bithumb.buy_limit_order("KRW-"+coin, 1715, 320))
+    print(bithumb.buy_limit_order("KRW-"+coin, price, amount))
 elif result["decision"] == "sell":
-    print(bithumb.sell_market_order("KRW-"+coin, my_coin))
+    # print(bithumb.sell_market_order("KRW-"+coin,my_coin-300))
+    print(bithumb.sell_limit_order("KRW-"+coin, price, amount))
